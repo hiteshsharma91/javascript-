98ba9964b2d911bb2995822e961c5bd6b07a9d32
hiteshsharma91 <hiteshsit11@gmail.com>
2025-03-01 18:25:42 +0000

    add new file

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144234
+let accountEmail = "hitesh@gmail.com";
+var password = "234534";
+accCity = "meerut";
+// accountId = 23432;
+
+accountEmail ="my@gmail.com";
+password = "a;dsfash";
+accCity = "noida"
+
+console.table([accountId, accountEmail, password, accCity])
