c9f335b9c81585c4b597fd49a572435a64f0f8e3
hiteshsharma91 <hiteshsit11@gmail.com>
2025-03-02 18:09:43 +0000

    Operations

--- basics/03_conveersionOperation.js
+++ basics/03_conveersionOperation.js
@@ -14,3 +14,23 @@ let StrNumber = String(Num);
 
 console.log(typeof StrNumber);
 console.log(StrNumber);
+
+// OPERATION --->>>
+
+let value = 5;
+let NegValue = -value
+console.log(NegValue);
+
+let str1 = "hello "
+let str2 = "hitesh"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
