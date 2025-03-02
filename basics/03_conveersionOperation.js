3d9ce407bd44db1e019a95260c93d5166ba50210
hiteshsharma91 <hiteshsit11@gmail.com>
2025-03-02 17:50:48 +0000

    convert dataype

---
+++ basics/03_conveersionOperation.js
@@ -0,0 +1,16 @@
+let score = "32";
+let score2 = "34sdf"
+
+console.log(typeof score);
+
+let valueINnumber = Number(score2);
+console.log(typeof valueINnumber);
+
+console.log(valueINnumber);
+
+let Num = 33;
+
+let StrNumber = String(Num);
+
+console.log(typeof StrNumber);
+console.log(StrNumber);
