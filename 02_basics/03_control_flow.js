490dc3c81fff5b4e4d0aae686b8b7b2e46842a77
hiteshsharma91 <hiteshsit11@gmail.com>
2025-03-09 04:02:41 +0000

    if else

---
+++ 02_basics/03_control_flow.js
@@ -0,0 +1,16 @@
+const userActive = true;
+if (userActive == true) {
+    console.log("user is active");
+    
+}
+const currentTemp = 30
+const  fixTemp = 50;
+
+if (currentTemp < fixTemp) {
+    console.log(`temp. is less than ${fixTemp}`);
+    
+}
+else{
+    console.log(`temp. is greater than ${fixTemp}`);
+    
+}
